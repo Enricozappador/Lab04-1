8eef0d39833b09443c88dab22dc2316b485e8fae
Enrico Zappador <s236559@studenti.polito.it>
2020-04-08 11:40:30 +0200

    Creata interfaccia grafica

--- Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
+++ Lab04_SegreteriaStudenti/src/main/java/it/polito/tdp/lab04/FXMLController.java
@@ -1,23 +1,95 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.lab04;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private ComboBox<?> CorsoCombo;
+
+    @FXML
+    private Button CercaIscrittibtn;
+
+    @FXML
+    private TextField Matricolatxt;
+
+    @FXML
+    private RadioButton Autofillbtn;
+
+    @FXML
+    private TextField Nometxt;
+
+    @FXML
+    private TextField Cognometxt;
+
+    @FXML
+    private Button CercaCorsibtn;
+
+    @FXML
+    private Button Iscrivibtn;
+
+    @FXML
+    private TextArea Resulttxt;
+
+    @FXML
+    private Button Resetbtn;
+
+    @FXML
+    void handleAutofill(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCercaCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleCercaIscritti(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleComboCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleIscrivi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleReset(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert CorsoCombo != null : "fx:id=\"CorsoCombo\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert CercaIscrittibtn != null : "fx:id=\"CercaIscrittibtn\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert Matricolatxt != null : "fx:id=\"Matricolatxt\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert Autofillbtn != null : "fx:id=\"Autofillbtn\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert Nometxt != null : "fx:id=\"Nometxt\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert Cognometxt != null : "fx:id=\"Cognometxt\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert CercaCorsibtn != null : "fx:id=\"CercaCorsibtn\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert Iscrivibtn != null : "fx:id=\"Iscrivibtn\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert Resulttxt != null : "fx:id=\"Resulttxt\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert Resetbtn != null : "fx:id=\"Resetbtn\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
